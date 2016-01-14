46727555d2b29dedce40f9a7b874688b76043319
Yemi Ajibola <ya9p8@mst.edu>
2016-01-14 13:31:22 -0600

    Added logic to buttons.

--- Calulatron/ViewController.m
+++ Calulatron/ViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *operandOne;
 @property (weak, nonatomic) IBOutlet UITextField *operandTwo;
 @property (weak, nonatomic) IBOutlet UILabel *operation;
+@property (weak, nonatomic) IBOutlet UILabel *resultsLabel;
 
 @end
 
@@ -36,12 +37,15 @@
     NSString *operation = self.operation.text;
     
     if ([operation isEqualToString:@"+"]) {
-        
+        self.resultsLabel.text = [NSString stringWithFormat:@"%.2f", opOne + opTwo];
     }else if ([operation isEqualToString:@"-"]) {
+        self.resultsLabel.text = [NSString stringWithFormat:@"%.2f", opOne - opTwo];
         
     } else if ([operation isEqualToString:@"x"]) {
+        self.resultsLabel.text = [NSString stringWithFormat:@"%.2f", opOne * opTwo];
         
     } else {
+        self.resultsLabel.text = [NSString stringWithFormat:@"%.2f", opOne / opTwo];
         
     }
 
